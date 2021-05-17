43b9a90d122862f37879325ba475ce5476f67507
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-05-17 11:22:17 +1000

    Updated files with getCalendarEvents function - Working

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,3 +1,46 @@
+module.exports.getAccessToken = async (event) => {
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id,
+		client_secret,
+		redirect_uris[0]
+	);
+
+	const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+	return new Promise((resolve, reject) => {
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) {
+				return reject(err);
+			}
+			return resolve(token);
+		});
+	})
+		.then((token) => {
+			// Respond with OAuth token
+			return {
+				statusCode: 200,
+				headers: {
+					'Access-Control-Allow-Headers': 'Content-Type',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Credentials': true,
+					'Access-Control-Allow-Methods': 'OPTIONs, POST, GET',
+				},
+				body: JSON.stringify(token),
+			};
+		})
+		.catch((err) => {
+			// Handle error
+			console.error(err);
+			return {
+				statusCode: 500,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+				body: JSON.stringify(err),
+			};
+		});
+};
+
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar('v3');
@@ -31,47 +74,70 @@ module.exports.getAuthURL = async () => {
 		scope: SCOPES,
 	});
 
-	module.exports.getAccessToken = async (event) => {
-		const oAuth2Client = new google.auth.OAuth2(
-			client_id,
-			client_secret,
-			redirect_uris[0]
-		);
-
-		const code = decodeURIComponent(`${event.pathParameters.code}`);
-
-		return new Promise((resolve, reject) => {
-			oAuth2Client.getToken(code, (err, token) => {
-				if (err) {
-					return reject(err);
-				}
-				return resolve(token);
-			});
-		})
-			.then((token) => {
-				// Respond with OAuth token
-				return {
-					statusCode: 200,
-					body: JSON.stringify(token),
-				};
-			})
-			.catch((err) => {
-				// Handle error
-				console.error(err);
-				return {
-					statusCode: 500,
-					body: JSON.stringify(err),
-				};
-			});
-	};
-
 	return {
 		statusCode: 200,
 		headers: {
+			'Access-Control-Allow-Headers': 'Content-Type',
 			'Access-Control-Allow-Origin': '*',
+			'Access-Cotrol-Allow-Credentials': true,
+			'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
 		},
 		body: JSON.stringify({
 			authUrl: authUrl,
 		}),
 	};
+};
+
+module.exports.getCalendarEvents = async (event) => {
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id,
+		client_secret,
+		redirect_uris[0]
+	);
+
+	const access_token = decodeURIComponent(
+		`${event.pathParameters.access_token}`
+	);
+	oAuth2Client.setCredentials({ access_token });
+
+	return new Promise((resolve, reject) => {
+		calendar.events.list(
+			{
+				calendarId: calendar_id,
+				auth: oAuth2Client,
+				timeMin: new Date().toISOString(),
+				singleEvents: true,
+				orderBy: 'startTime',
+			},
+			(error, response) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(response);
+				}
+			}
+		);
+	})
+		.then((results) => {
+			return {
+				statusCode: 200,
+				headers: {
+					'Access-Control-Allow-Headers': 'Content-Type',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Credentials': true,
+					'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+				},
+				body: JSON.stringify({ events: results.data.items }),
+			};
+		})
+		.catch((err) => {
+			console.error(err);
+			return {
+				statusCode: 500,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+				body: JSON.stringify(err),
+			};
+		});
 };
