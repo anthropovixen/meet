599080e9920757e15f2a2f98c555a73635171556
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-05-26 22:27:00 +1000

    Clean useless imports

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Event from '../Event';
-import { mockData, mockEvent } from '../mock-data';
+import { mockData } from '../mock-data';
 import { extractEvents } from '../api';
 
 const eventData = extractEvents(mockData);
