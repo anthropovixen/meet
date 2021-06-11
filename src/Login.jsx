57dbea04efbb79bf133f71e4f30a6c74050719be
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-06-11 20:35:55 +1000

    update files

--- src/Login.jsx
+++ src/Login.jsx
@@ -25,7 +25,9 @@ function Login() {
 					</a>
 				</div>
 			</div>
-			<a href="./privacy.html">Privacy Policy</a>
+			<a href="/privacy.html" rel="nofollow noopener">
+				Privacy Policy
+			</a>
 		</div>
 	);
 }
