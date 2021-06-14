4dd297b7c3625eaeeaa25dcac47f196c795ecade
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-06-14 11:03:12 +1000

    Update Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -35,11 +35,11 @@ class ErrorAlert extends Alert {
 	}
 }
 
-class OfflineAlert extends Alert {
+class WarningAlert extends Alert {
 	constructor(props) {
 		super(props);
 		this.color = 'orange';
 	}
 }
 
-export { InfoAlert, ErrorAlert, OfflineAlert };
+export { InfoAlert, ErrorAlert, WarningAlert };
