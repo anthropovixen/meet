936091dbf7a92b5a7f6657cf3d2b315af538be65
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-06-18 14:53:14 +1000

    Added ResponsiveContainer to make chart responsive

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
 	Tooltip,
 	XAxis,
 	YAxis,
+	ResponsiveContainer,
 } from 'recharts';
 
 class App extends Component {
@@ -118,22 +119,22 @@ class App extends Component {
 				/>
 				<WarningAlert text={this.state.warningText} />
 				<h4>Events in each city</h4>
-				<ScatterChart
-					width={800}
-					height={400}
-					margin={{
-						top: 20,
-						right: 20,
-						bottom: 20,
-						left: 20,
-					}}
-				>
-					<CartesianGrid />
-					<XAxis type="category" dataKey="city" name="city" />
-					<YAxis type="number" dataKey="number" name="number of events" />
-					<Tooltip cursor={{ strokeDasharray: '3 3' }} />
-					<Scatter data={this.getData()} fill="#8884d8" />
-				</ScatterChart>
+				<ResponsiveContainer height={400}>
+					<ScatterChart
+						margin={{
+							top: 20,
+							right: 20,
+							bottom: 20,
+							left: 20,
+						}}
+					>
+						<CartesianGrid />
+						<XAxis type="category" dataKey="city" name="city" />
+						<YAxis type="number" dataKey="number" name="number of events" />
+						<Tooltip cursor={{ strokeDasharray: '3 3' }} />
+						<Scatter data={this.getData()} fill="#8884d8" />
+					</ScatterChart>
+				</ResponsiveContainer>
 				<EventList events={this.state.events} />
 			</div>
 		);
